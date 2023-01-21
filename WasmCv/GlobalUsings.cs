1e04c0517df8c366865b0a7b6ee2dd8307f4de72
W4TR1X <30633382+W4TR1X@users.noreply.github.com>
2023-01-21 19:04:41 +0300

    * edited

---
+++ WasmCv/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using MudBlazor.Services;
+global using WasmCv;
+global using WasmCv.Services;
---
+++ WasmCv/Services/PageInfoService.cs
@@ -0,0 +1,24 @@
+﻿namespace WasmCv.Services;
+
+public class PageInfoService
+{
+    public const string BASE_TITLE = "Mustafa Dağcı | Software Developer";
+    public string AppTitle { get; private set; } = BASE_TITLE;
+    public string? PageTitle { get; private set; }
+    public Action<string>? AppTitleChanged { get; set; }
+
+    public void SetPageTitle(string? title)
+    {
+        if (title != null)
+        {
+            AppTitle = title + " | " + BASE_TITLE;
+        }
+        else
+        {
+            AppTitle = BASE_TITLE;
+        }
+
+        PageTitle = title;
+        AppTitleChanged?.Invoke(AppTitle);
+    }
+}
